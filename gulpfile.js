72c868c972d9b9de62cf33bc6d892058efd3bd11
blankov <blankov_v@icloud.com>
2020-09-21 02:58:15 +0300

    Оставил только less и stylus препроцессоры. Оставил sass/scss.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
 // VARIABLES & PATHS
 
 const preprocessor = 'scss'; // Preprocessor (sass, scss, less, styl)
-const fileswatch = 'html,htm,txt,json,md,woff2'; // List of files extensions for watching & hard reload (comma separated)
-const imageswatch = 'jpg,jpeg,png,webp,svg'; // List of images extensions for watching & compression (comma separated)
-const baseDir = 'app'; // Base directory path without «/» at the end
-const online = true; // If «false» - Browsersync will work offline without internet connection
+const fileswatch   = 'html,htm,txt,json,md,woff2'; // List of files extensions for watching & hard reload (comma separated)
+const imageswatch  = 'jpg,jpeg,png,webp,svg'; // List of images extensions for watching & compression (comma separated)
+const baseDir      = 'app'; // Base directory path without «/» at the end
+const online       = true; // If «false» - Browsersync will work offline without internet connection
 
 const paths = {
 
@@ -17,51 +17,56 @@ const paths = {
    },
 
    styles: {
-      src: `${baseDir}/${preprocessor}/main.*`,
+      src:  `${baseDir}/${preprocessor}/main.*`,
       dest: `${baseDir}/css`,
    },
 
    images: {
-      src: `${baseDir}/images/src/**/*`,
+      src:  `${baseDir}/images/src/**/*`,
       dest: `${baseDir}/images/dest`,
    },
 
    deploy: {
-      hostname: 'username@yousite.com', // Deploy hostname
+      hostname:    'username@yousite.com', // Deploy hostname
       destination: 'yousite/public_html/', // Deploy destination
-      include: [/* '*.htaccess' */], // Included files to deploy
-      exclude: ['**/Thumbs.db', '**/*.DS_Store'], // Excluded files from deploy
+      include:     [], // Included files to deploy
+      exclude:     ['**/Thumbs.db', '**/*.DS_Store'], // Excluded files from deploy
    },
 
    cssOutputName: 'app.min.css',
-   jsOutputName: 'app.min.js',
+   jsOutputName:  'app.min.js',
 
 };
 
 // LOGIC
 
 const {
-   src, dest, parallel, series, watch,
+   src,
+   dest,
+   parallel,
+   series,
+   watch,
 } = require('gulp');
-/* eslint-disable no-unused-vars */
-const sass = require('gulp-sass');
-const scss = require('gulp-sass');
-/* eslint-disable no-unused-vars */
-const cleancss = require('gulp-clean-css');
-const concat = require('gulp-concat');
-const browserSync = require('browser-sync').create();
-const uglify = require('gulp-uglify-es').default;
+// const sass = require('gulp-sass');
+const scss         = require('gulp-sass');
+const cleancss     = require('gulp-clean-css');
+const concat       = require('gulp-concat');
+const browserSync  = require('browser-sync').create();
+const uglify       = require('gulp-uglify-es').default;
 const autoprefixer = require('gulp-autoprefixer');
-const imagemin = require('gulp-imagemin');
-const newer = require('gulp-newer');
-const rsync = require('gulp-rsync');
-const del = require('del');
+const imagemin     = require('gulp-imagemin');
+const newer        = require('gulp-newer');
+const rsync        = require('gulp-rsync');
+const del          = require('del');
 
 function browsersync() {
    browserSync.init({
-      server: { baseDir: `${baseDir}/` },
-      notify: false,
+      server: {
+         baseDir: `${baseDir}/`,
+      },
+      notify:  false,
       online,
+      browser: ['google chrome', 'firefox'],
    });
 }
 
@@ -75,11 +80,22 @@ function scripts() {
 
 function styles() {
    return src(paths.styles.src)
-      // eslint-disable-next-line no-eval
-      .pipe(eval(preprocessor)())
+      /* eslint-disable-next-line no-eval */
+      // .pipe(eval(preprocessor)())
+      .pipe(scss())
       .pipe(concat(paths.cssOutputName))
-      .pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true }))
-      .pipe(cleancss({ level: { 1: { specialComments: 0 } } /* format: 'beautify' */ }))
+      .pipe(autoprefixer({
+         overrideBrowserslist: ['last 10 versions'],
+         grid:                 true,
+      }))
+      .pipe(cleancss({
+         level: {
+            1: {
+               specialComments: 0,
+            },
+         },
+         /* format: 'beautify' */
+      }))
       .pipe(dest(paths.styles.dest))
       .pipe(browserSync.stream());
 }
@@ -92,36 +108,46 @@ function images() {
 }
 
 function cleanimg() {
-   return del(`${paths.images.dest}/**/*`, { force: true });
+   return del(`${paths.images.dest}/**/*`, {
+      force: true,
+   });
 }
 
 function deploy() {
    return src(`${baseDir}/`)
       .pipe(rsync({
-         root: `${baseDir}/`,
-         hostname: paths.deploy.hostname,
+         root:        `${baseDir}/`,
+         hostname:    paths.deploy.hostname,
          destination: paths.deploy.destination,
-         include: paths.deploy.include,
-         exclude: paths.deploy.exclude,
-         recursive: true,
-         archive: true,
-         silent: false,
-         compress: true,
+         include:     paths.deploy.include,
+         exclude:     paths.deploy.exclude,
+         recursive:   true,
+         archive:     true,
+         silent:      false,
+         compress:    true,
       }));
 }
 
 function startwatch() {
-   watch(`${baseDir}/${preprocessor}/**/*`, { usePolling: true }, styles);
-   watch(`${baseDir}/images/src/**/*.{${imageswatch}}`, { usePolling: true }, images);
-   watch(`${baseDir}/**/*.{${fileswatch}}`, { usePolling: true }).on('change', browserSync.reload);
-   watch([`${baseDir}/js/**/*.js`, `!${paths.scripts.dest}/*.min.js`], { usePolling: true }, scripts);
+   watch(`${baseDir}/${preprocessor}/**/*`, {
+      usePolling: true,
+   }, styles);
+   watch(`${baseDir}/images/src/**/*.{${imageswatch}}`, {
+      usePolling: true,
+   }, images);
+   watch(`${baseDir}/**/*.{${fileswatch}}`, {
+      usePolling: true,
+   }).on('change', browserSync.reload);
+   watch([`${baseDir}/js/**/*.js`, `!${paths.scripts.dest}/*.min.js`], {
+      usePolling: true,
+   }, scripts);
 }
 
 exports.browsersync = browsersync;
-exports.assets = series(cleanimg, styles, scripts, images);
-exports.styles = styles;
-exports.scripts = scripts;
-exports.images = images;
-exports.cleanimg = cleanimg;
-exports.deploy = deploy;
-exports.default = parallel(images, styles, scripts, browsersync, startwatch);
+exports.assets      = series(cleanimg, styles, scripts, images);
+exports.styles      = styles;
+exports.scripts     = scripts;
+exports.images      = images;
+exports.cleanimg    = cleanimg;
+exports.deploy      = deploy;
+exports.default     = parallel(images, styles, scripts, browsersync, startwatch);
